b9cbed0e308761a08a24996323db729e2dda5a17
Mstone8370 <mstone8370@gmail.com>
2025-05-23 15:37:27 +0900

    feat: DepthOfFieldRenderPass 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/PostProcess/DepthOfFieldRenderPass.cpp
@@ -0,0 +1,31 @@
+﻿#include "DepthOfFieldRenderPass.h"
+
+void FDepthOfFieldRenderPass::Initialize(FDXDBufferManager* InBufferManager, FGraphicsDevice* InGraphics, FDXDShaderManager* InShaderManage)
+{
+    FRenderPassBase::Initialize(InBufferManager, InGraphics, InShaderManage);
+}
+
+void FDepthOfFieldRenderPass::PrepareRenderArr()
+{
+    FRenderPassBase::PrepareRenderArr();
+}
+
+void FDepthOfFieldRenderPass::ClearRenderArr()
+{
+    FRenderPassBase::ClearRenderArr();
+}
+
+void FDepthOfFieldRenderPass::Render(const std::shared_ptr<FEditorViewportClient>& Viewport)
+{
+    PrepareRender(Viewport);
+
+    CleanUpRender(Viewport);
+}
+
+void FDepthOfFieldRenderPass::PrepareRender(const std::shared_ptr<FEditorViewportClient>& Viewport)
+{
+}
+
+void FDepthOfFieldRenderPass::CleanUpRender(const std::shared_ptr<FEditorViewportClient>& Viewport)
+{
+}
