b2e5215c0c2aa18be33d217fc709852e4dc4a45c
ywj <woojae0107@gmail.com>
2025-06-02 20:37:28 +0900

    fix : 스카이스피어 카메라 위치 동기화

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Actors/SkySphereActor.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Actors/SkySphereActor.cpp
@@ -1,12 +1,22 @@
 #include "SkySphereActor.h"
 #include "Components/StaticMeshComponent.h"
 #include "Engine/FObjLoader.h"
+#include "Editor/LevelEditor/SLevelEditor.h"
+#include "Editor/UnrealEd/EditorViewportClient.h"
 
 ASkySphere::ASkySphere()
 {
-    SkySphereMesh = FObjManager::GetStaticMesh(L"Contents/Sphere.obj");
-    //SkySphereMesh->GetRenderData()->
-    RootComponent = nullptr;
+
+}
+
+void ASkySphere::PostSpawnInitialize()
+{
+    UStaticMesh* SkySphereMesh = FObjManager::GetStaticMesh(L"Contents/Skysphere/skysphere.obj");
+    SkySphereMeshComp = AddComponent<UStaticMeshComponent>(L"SkySphereMeshComp");
+    SkySphereMeshComp->SetStaticMesh(SkySphereMesh);
+    RootComponent = SkySphereMeshComp;
+
+    SetActorScale(FVector(50000.0f, 50000.0f, 50000.0f));
 }
 
 UObject* ASkySphere::Duplicate(UObject* InOuter)
@@ -14,10 +24,17 @@ UObject* ASkySphere::Duplicate(UObject* InOuter)
     ThisClass* NewActor = Cast<ThisClass>(Super::Duplicate(InOuter));
     if (!NewActor) return nullptr;
 
+    NewActor->SkySphereMeshComp = NewActor->GetComponentByClass<UStaticMeshComponent>();
     return NewActor;
 }
 
 void ASkySphere::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+    auto VPC = GEngineLoop.GetLevelEditor()->GetActiveViewportClient().get();
+    if (VPC)
+    {
+        const FVector CameraLocation = VPC->GetCameraLocation();
+        SetActorLocation(CameraLocation);
+    }
 }
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Actors/SkySphereActor.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Actors/SkySphereActor.h
@@ -2,15 +2,16 @@
 #include "GameFramework/Actor.h"
 
 class UStaticMesh;
+class UStaticMeshComponent;
 class ASkySphere : public AActor
 {
     DECLARE_CLASS(ASkySphere, AActor)
 public:
     ASkySphere();
-
+    virtual void PostSpawnInitialize() override;
     virtual UObject* Duplicate(UObject* InOuter);
     virtual void Tick(float DeltaTime) override;
 
 protected:
-    UPROPERTY_WITH_FLAGS(EditAnywhere, UStaticMesh*, SkySphereMesh, = nullptr)
+    UPROPERTY_WITH_FLAGS(EditAnywhere, UStaticMeshComponent*, SkySphereMeshComp, = nullptr)
 };
